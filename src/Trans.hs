e54d684efadd91854547cac38d5e6726a854723f
Geoff Hamilton <hamilton@computing.dcu.ie>
2024-05-30 14:16:01 +0100

    Tidied up code

--- src/Trans.hs
+++ src/Trans.hs
@@ -11,11 +11,10 @@ import Data.Tuple
              
 -- deforester
 
-deforest (t,d) = let (t',d') = returnval (trans 1 t EmptyCtx (free t) [] [] [] d [])
+deforest (t,d) = let t' = returnval (def t EmptyCtx (free t) [] [] [] d [])
                  in  residualise t'
 
--- function: trans n t k fv m s1 s2 d
--- n: transformation level
+-- function: def t k fv m s1 s2 d e
 -- t: current term
 -- k: context
 -- fv: free variables
@@ -23,91 +22,75 @@ deforest (t,d) = let (t',d') = returnval (trans 1 t EmptyCtx (free t) [] [] [] d
 -- s1: generalisation environment
 -- s2: generalisation environment
 -- d: function definitions
+-- e: previously transformed terms
 
-trans 0 t k fv m s1 s2 d1 d2 = return (place t k,[])
+def (Free x) k fv m s1 s2 d e = defCtx (Free x) k fv m s1 s2 d e
+def (Lambda x t) EmptyCtx fv m s1 s2 d e = let x' = renameVar fv x
+                                           in  do
+                                               t' <- def (concrete x' t) EmptyCtx (x':fv) m s1 s2 d e
+                                               return (Lambda x' (abstract t' x'))
+def (Lambda x t) (ApplyCtx k u) fv m s1 s2 d e = def (subst u t) k fv m s1 s2 d e
+def (Lambda x t) (CaseCtx k bs) fv m s1 s2 d e = error "Unapplied function in case selector"
+def (Con c ts) EmptyCtx fv m s1 s2 d e = do
+                                         ts' <- defArgs ts fv m s1 s2 d e
+                                         return (Con c ts')
+def (Con c ts) (ApplyCtx k u) fv m s1 s2 d e = error ("Constructor application is not saturated: "++show (place (Con c ts) (ApplyCtx k u)))
+def (Con c ts) (CaseCtx k bs) fv m s1 s2 d e = case find (\(c',xs,t) -> c==c' && length xs == length ts) bs of
+                                                  Nothing -> error ("No matching pattern in case for term:\n\n"++show (Case (Con c ts) bs))
+                                                  Just (c',xs,t) -> def (foldr subst t ts) k fv m s1 s2 d e
+def (Apply t u) k fv m s1 s2 d e = def t (ApplyCtx k u) fv m s1 s2 d e
+def (Case t bs) k fv m s1 s2 d e = def t (CaseCtx k bs) fv m s1 s2 d e
+def (Fun f) k fv m s1 s2 d e = let t = place (Fun f) k
+                               in  case [rename r u | u@(Unfold _ t' _) <- e, r <- renaming t' t] of
+                                      (u:_) -> return u
+                                      [] -> fold t fv m s1 s2 d e
+def (Let x t u) k fv m s1 s2 d e = let x' = renameVar fv x
+                                   in  do
+                                       t' <- def t EmptyCtx fv m s1 s2 d e
+                                       u' <- def (concrete x' u) k (x':fv) m s1 s2 d (unfolds t'++e)
+                                       return (Let x t' (abstract u' x'))
 
-trans n (Free x) k fv m s1 s2 d1 d2 = transCtx n (Free x) k fv m s1 s2 d1 d2
-trans n (Lambda x t) EmptyCtx fv m s1 s2 d1 d2 = let x' = renameVar fv x
-                                                 in  do
-                                                     (t',d2') <- trans n (concrete x' t) EmptyCtx (x':fv) m s1 s2 d1 d2
-                                                     return (Lambda x' (abstract t' x'),d2')
-trans n (Lambda x t) (ApplyCtx k u) fv m s1 s2 d1 d2 = trans n (subst u t) k fv m s1 s2 d1 d2
-trans n (Lambda x t) (CaseCtx k bs) fv m s1 s2 d1 d2 = error "Unapplied function in case selector"
-trans n (Con c ts) EmptyCtx fv m s1 s2 d1 d2 = do
-                                               (ts',d2') <- transArgs n ts fv m s1 s2 d1 d2
-                                               return (Con c ts',d2')
-trans n (Con c ts) (ApplyCtx k u) fv m s1 s2 d1 d2 = error ("Constructor application is not saturated: "++show (place (Con c ts) (ApplyCtx k u)))
-trans n (Con c ts) (CaseCtx k bs) fv m s1 s2 d1 d2 = case find (\(c',xs,t) -> c==c' && length xs == length ts) bs of
-                                                        Nothing -> error ("No matching pattern in case for term:\n\n"++show (Case (Con c ts) bs))
-                                                        Just (c',xs,t) -> trans n (foldr subst t ts) k fv m s1 s2 d1 d2
-trans n (Apply t u) k fv m s1 s2 d1 d2 = trans n t (ApplyCtx k u) fv m s1 s2 d1 d2
-trans n (Case t bs) k fv m s1 s2 d1 d2 = trans n t (CaseCtx k bs) fv m s1 s2 d1 d2
-trans n (Fun f) k fv m s1 s2 d1 d2 = fold n (place (Fun f) k) fv m s1 s2 d1 d2
-trans n (Let x t u) k fv m s1 s2 d1 d2 = trans n (subst t u) k fv m s1 s2 d1 d2
-trans n (Unfold t u) k fv m s1 s2 d1 d2 = fold n (place (Unfold t u) k) fv m s1 s2 d1 d2
-trans n (Fold t u) k fv m s1 s2 d1 d2 = do
-                                        (t',_) <- trans (n-1) u k fv [] [] [] d1 []
-                                        fold n t' fv m s1 s2 d1 d2
+defArgs [] fv m s1 s2 d e = return []
+defArgs (t:ts) fv m s1 s2 d e = do
+                                t' <- def t EmptyCtx fv m s1 s2 d e
+                                ts' <- defArgs ts fv m s1 s2 d (unfolds t'++e)
+                                return (t':ts')
 
-transArgs n [] fv m s1 s2 d1 d2 = return ([],d2)
-transArgs n (t:ts) fv m s1 s2 d1 d2 = do
-                                      (t',d2') <- trans n t EmptyCtx fv m s1 s2 d1 d2
-                                      (ts',d2'') <- transArgs n ts fv m s1 s2 d1 d2'
-                                      return (t':ts',d2'')
-transBranches n [] k fv m s1 s2 d1 d2 = return ([],d2)
-transBranches n ((c,xs,t):bs) k fv m s1 s2 d1 d2 = let xs' = renameVars fv xs
-                                                   in do
-                                                      (t',d2') <- trans n (foldr concrete t xs') k (xs'++fv) m s1 s2 d1 d2
-                                                      (bs',d2'') <- transBranches n bs k fv m s1 s2 d1 d2'
-                                                      return ((c,xs,foldl abstract t' xs'):bs',d2'')
+defBranches [] k fv m s1 s2 d e = return []
+defBranches ((c,xs,t):bs) k fv m s1 s2 d e = let xs' = renameVars fv xs
+                                             in do
+                                                t' <- def (foldr concrete t xs') k (xs'++fv) m s1 s2 d e
+                                                bs' <- defBranches bs k fv m s1 s2 d (unfolds t'++e)
+                                                return ((c,xs,foldl abstract t' xs'):bs')
 
--- auxiliary function: transCtx n t k fv m s1 s2 d
--- n: transformation level
--- t: current term
--- k: context
--- fv: free variables
--- m: memoised terms
--- s1: generalisation environment
--- s2: generalisation environment
--- d: function definitions
+defCtx t EmptyCtx fv m s1 s2 d e = return t
+defCtx t (ApplyCtx k u) fv m s1 s2 d e = do
+                                         u' <- def u EmptyCtx fv m s1 s2 d e
+                                         defCtx (Apply t u') k fv m s1 s2 d (unfolds u'++e)
+defCtx t (CaseCtx k bs) fv m s1 s2 d e = do
+                                         bs' <- defBranches bs k fv m s1 s2 d e
+                                         return (Case t bs')
 
-transCtx n t EmptyCtx fv m s1 s2 d1 d2 = return (t,d2)
-transCtx n t (ApplyCtx k u) fv m s1 s2 d1 d2 = do
-                                               (u',d2') <- trans n u EmptyCtx fv m s1 s2 d1 d2
-                                               transCtx n (Apply t u') k fv m s1 s2 d1 d2'
-transCtx n t (CaseCtx k bs) fv m s1 s2 d1 d2 = do
-                                               (bs',d2') <- transBranches n bs k fv m s1 s2 d1 d2
-                                               return (Case t bs',d2')
+defFold [] fv m s1 s2 d e = return []
+defFold ((x,t):s) fv m s1 s2 d e = do
+                                   t' <- def t EmptyCtx fv m s1 s2 d e
+                                   s' <- defFold s fv m s1 s2 d (unfolds t'++e)
+                                   return ((x,t'):s')
 
-transGen n (t,[],[]) fv m s1 s2 d1 d2 = trans n t EmptyCtx fv m s1 s2 d1 d2
-transGen n (t,(x,t'):s1',(_,t''):s2') fv m s1 s2 d1 d2 = case [x' | (x',u) <- s1, (x'',u') <- s2, x'==x'' && u==t' && u'==t''] of
-                                                            (x':_) -> transGen n (rename [(x,x')] t,s1',s2') (x':fv) m s1 s2 d1 d2
-                                                            [] -> do
-                                                                  (u,d2') <- trans n t' EmptyCtx fv m s1 s2 d1 d2
-                                                                  (u',d2'') <- transGen n (t,s1',s2') (x:fv) m ((x,t'):s1) ((x,t''):s2) d1 d2'
-                                                                  return (Let x u (abstract u' x),d2'')
+fold t fv m s1 s2 d e = case [(u,t',r) | (u,t') <- m, r <- embedding t' t] of
+                           ((u,t',r):_) -> let (u',s1',s2') = generalise t' t fv s1 s2
+                                           in  case renaming t' (rename r u') of
+                                                  [] -> throw (u,(u',s1',s2'))
+                                                  (r:_) -> do
+                                                           s <- defFold s2' fv m s1 s2 d e
+                                                           return (Fold (instantiate s (rename r u)))
+                           [] -> let f = renameVar (fv ++ [f | (Unfold t _ _) <- e, let Fun f = redex t]) "f"
+                                     xs = free t
+                                     u = foldl (\t x -> Apply t (Free x)) (Fun f) xs
+                                     handler (u',(t',s1',s2')) = if   u==u'
+                                                                 then def (makeLet s1' t') EmptyCtx (f:fv) m (s1++s1') (s2++s2') d e                                                               
+                                                                 else throw (u',(t',s1',s2'))
+                                   in  do
+                                       t' <- handle (def (unfold t d) EmptyCtx (f:fv) ((u,t):m) s1 s2 d e) handler
+                                       return (Unfold u t t')
 
-transFold n [] fv m s1 s2 d1 d2 = return ([],d2)
-transFold n ((x,t):s) fv m s1 s2 d1 d2 = do
-                                         (t',d2') <- trans n t EmptyCtx fv m s1 s2 d1 d2
-                                         (s',d2'') <- transFold n s fv m s1 s2 d1 d2'
-                                         return ((x,t'):s',d2'')
-
-fold n t fv m s1 s2 d1 d2 = case [(f,xs,t') | (f,(xs,t')) <- m, r <- embedding t' t] of
-                              ((f,xs,t'):_) -> let (u,s1',s2') = generalise [] t' t fv [] [] []
-                                               in  case renaming t' u of
-                                                      [] -> throw (t',(u,s1',s2'))
-                                                      (r:rs) -> do
-                                                                (s,d2') <- transFold n s2' fv m s1 s2 d1 d2
-                                                                return (Fold (instantiate s (rename r (foldl (\t x -> Apply t (Free x)) (Fun f) xs))) t,d2')
-                              [] -> case [rename r u | (f,(t',u)) <- d2, r <- renaming t' t] of
-                                       (u':_) -> return (u',d2)
-                                       [] -> let f = renameVar (map fst m ++ map fst d1 ++ map fst d2) "f"
-                                                 xs = free t
-                                                 t' = foldl (\t x -> Apply t (Free x)) (Fun f) xs
-                                                 handler (t',(u,s1',s2')) = if   t==t'
-                                                                            then transGen n (u,s1',s2') fv m s1 s2 d1 d2                                                                
-                                                                            else throw (t',(u,s1',s2'))
-                                             in  do
-                                                 (u',d2') <- handle (trans n (unfold t d1) EmptyCtx fv ((f,(xs,t)):m) s1 s2 d1 d2) handler
-                                                 return (if Fun f `elem` folds u' then (Unfold t' u',(f,(t,Unfold t' u')):d2') else (u',d2'))
